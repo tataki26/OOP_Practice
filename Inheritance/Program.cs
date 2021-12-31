1a70b99a5d0650ee7ad3011eaa22d9636ab44176
tataki26 <tataki26@gmail.com>
2021-12-31 12:19:00 +0900

    Add Inheritance and Override project

---
+++ Inheritance/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Example
+    {
+        public void Print()
+        {
+            Console.WriteLine("Call Print function");
+        }
+    }
+    // Example 클래스를 상속한 CopyExample(파생 클래스)
+    class CopyExample : Example
+    {
+        public void Print2()
+        {
+            Console.WriteLine("Call Print2 function");
+        }
+
+        // 반환자, 함수명, 파라미터를 같은 이름으로 하면 함수가 재정의(override)된다
+        // 접근 제한자 뒤에는 재정의 키워드 new를 넣는다(상위 클래스의 일반 함수를 재정의 할 경우)
+        // 함수를 재정의 하면 상의 클래스 함수가 아닌 재정의된 함수가 호출된다
+        public new void Print()
+        {
+            Console.WriteLine("Call new Print function");
+        }
+
+        /*
+        // 상위 클래스에서 재정의를 허락한 경우(abstract와 virtual)에는 override 키워드를 사용한다
+        // virtual이 없는 상태에서 override를 사용하면 에러 발생
+        public override void Print()
+        {
+            Console.WriteLine("Call new print function");
+        }
+        */
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CopyExample ce = new CopyExample();
+            // Example 클래스를 상속받았기 때문에 내부에서 함수를 정의하지 않아도 사용 가능하다
+            ce.Print();
+            // 파생 클래스에서 추가된 함수
+            ce.Print2();
+
+            Console.Write("Press any key...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Override/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Override
+{
+    class Example
+    {
+        // 가상 함수
+        public virtual void Print()
+        {
+            Console.WriteLine("Call Print function");
+        }
+    }
+    // Example 클래스 상속
+    class CopyExample1 : Example
+    {
+        // override 재정의
+        public override void Print()
+        {
+            Console.WriteLine("Call new function");
+        }
+    }
+    class CopyExample2 : Example
+    {
+        // new 재정의
+        public new void Print()
+        {
+            Console.WriteLine("Call new Print function");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example 변수 타입으로 CopyExample1과 CopyExample2 클래스 인스턴스 생성
+            Example ce1 = new CopyExample1();
+            ce1.Print(); // 파생 클래스 함수 호출(HEAP에 들어간 인스턴스의 Print 함수 호출)
+
+            Example ce2 = new CopyExample2();
+            // 파생 클래스에서 추가된 함수
+            ce2.Print(); // ***Example(상위) 클래스*** 함수 호출 = 재정의 실패
+            // 변수 타입에 따라 가르키는 함수가 달라진다(Example과 CopyExample2)
+
+            Console.Write("Press any key...");
+            Console.ReadLine();
+        }
+    }
+}
