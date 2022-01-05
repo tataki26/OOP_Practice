993922ec94cd37b1732647fb752c526437db5f36
tataki26 <tataki26@gmail.com>
2022-01-05 15:35:28 +0900

    Update Abstract class and Create Virtual class
    - Add Example2 class to make Abstract class array

--- Abstract/Program.cs
+++ Abstract/Program.cs
@@ -3,7 +3,7 @@
 namespace Abstract
 {
     // 추상 클래스 생성
-    abstract class AbstractExample
+    abstract class AbstractExample // 많은 클래스의 공통 부분을 묶어서 공통 클래스를 만드는 데 사용
     {
         public void Print()
         {
@@ -16,21 +16,46 @@ namespace Abstract
         protected abstract string GetData(); // 함수 선언문                                      
     }
     // 추상 클래스 상속
-    class Example : AbstractExample
+    class Example1 : AbstractExample
     {
         // 추상 클래스에서 GetData가 완전히 구현되지 않았기 때문에 상속 받는 클래스에서 반드시 재정의 해야 한다
         protected override string GetData()
         {
-            return "Hello World";
+            return "Example1";
         }
 
     }
+    class Example2 : AbstractExample
+    {
+        protected override string GetData()
+        {
+            return "Example2";
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            /*
+            // 가상 클래스의 인스턴스를 직접 생성하지 않고 그를 상속 받은 파생 클래스의 인스턴스를 생성한다
             Example ex = new Example();
             ex.Print();
+            */
+
+            // 추상 클래스의 지시자로 배열을 선언
+            AbstractExample[] array = new AbstractExample[]
+            {
+                new Example1(), // Example1 클래스의 인스턴스 생성
+                new Example2() // Example2 클래스의 인스턴스 생성
+            };
+
+            // 인스턴스를 반복문으로 출력
+            foreach (AbstractExample ex in array)
+            {
+                // 지시자는 추상 클래스 AbstractExample(Stack)이지만, Heap에는 Example1 또는 Example2의 클래스의 인스턴스가 할당되어 있다
+                ex.Print(); // 인스턴스에서 GetData 함수를 재정의하여 나온 결과로 출력
+            }
 
             Console.Write("Press any key...");
             Console.ReadLine();
